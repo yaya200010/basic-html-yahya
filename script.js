bde1ab1e9cf981031aad775391c5aeed1fe95697
Nithi Prasanpanich <71967328+note5lab@users.noreply.github.com>
2023-03-22 01:12:41 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 10;
+console.log(a + b);
+
+document.getElementById("main-head").innerHTML = a + b;
+
+const test = document.getElementById("main-head");
+console.log(test.innerHTML);
